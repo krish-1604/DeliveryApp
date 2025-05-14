2564b3704378797c9d50b4bd611d6fb54000bf04
Utpal <dev.theutpal@gmail.com>
2025-05-15 02:31:30 +0530

    feat(global): add basic required comoponents with thier types included

---
+++ components/Auth/Background.tsx
@@ -0,0 +1,28 @@
+import { Image, View } from 'react-native';
+import { Body, Heading } from '../UI/typography';
+
+import authImage from '../../assets/images/auth/obj.png';
+const Background = () => {
+	return (
+		<View className="w-full relative h-4/6">
+			<View className="absolute -top-1/4 scale-[170%] overflow-hidden left-0 w-screen h-full rounded-br-[50%] rounded-tr-[60%] rotate-[60deg] bg-gradient-to-br from-primary/80 via-[50%] via-primary/70 to-primary/5 "></View>
+			<View className="flex px-5 justify-center gap-5 items-center w-full h-full">
+				<Image
+					source={authImage}
+					// source={require('../../assets/images/auth/obj.png')}
+					className="flex justify-center align-middle h-full w-auto object-cover aspect-square"
+				/>
+				<View className="absolute top-10 right-16 bg-white rounded-full w-1 h-1"></View>
+				<View className="absolute top-16 right-1/2 bg-white rounded-full w-1 h-1"></View>
+				<View className="absolute top-10 left-1/4 bg-white rounded-full w-1 h-1"></View>
+				<View className="absolute top-1/4 left-16 bg-white rounded-full w-1 h-1"></View>
+				<View className="flex w-full">
+					<Body text="Be a Himalayan Droneshala Delivery Partner" />
+					<Heading text="Get a stable monthly income" />
+				</View>
+			</View>
+		</View>
+	);
+};
+
+export default Background;
---
+++ components/UI/button.tsx
@@ -0,0 +1,28 @@
+import { ButtonType } from '@/utils/types';
+import { TouchableHighlight, TouchableOpacity } from 'react-native';
+
+const ButtonHighlight = ({ className, disabled = false, onPress, children }: ButtonType) => {
+	return (
+		<TouchableHighlight
+			className={`${className} w-full h-12 bg-primary rounded-full flex items-center justify-center`}
+			onPress={onPress}
+			disabled={disabled}
+		>
+			{children}
+		</TouchableHighlight>
+	);
+};
+
+const ButtonOpacity = ({ className, disabled = false, onPress, children }: ButtonType) => {
+	return (
+		<TouchableOpacity
+			className={`${className} w-full h-12 bg-primary rounded-full flex items-center justify-center`}
+			onPress={onPress}
+			disabled={disabled}
+		>
+			{children}
+		</TouchableOpacity>
+	);
+};
+
+export { ButtonHighlight, ButtonOpacity };
